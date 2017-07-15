ac53c07dfcfc592d2a157140978f36b6bcacbeb4
blackeyedhuman <blackeyed9human@gmail.com>
2017-07-15 11:28:21 +0530

    Add files via upload

---
+++ Update.py
@@ -0,0 +1,71 @@
+import csv
+
+def Update_Product( Product ):
+
+    #opening file
+    #@Sample_Space = open("test.csv", "r")
+    #contents = csv.DictReader(Sample_Space)
+
+    with open ("test.csv", "r") as Sample_Space:
+        Sample_Space_Reader =  csv.DictReader(Sample_Space)
+
+        #for row in contents:
+        # if int(content['quantity']) > 100:
+            #print(iterator("id"))
+
+        print("Does the product exist? Enter y for yes or n for no ")
+        opt = input();
+
+        if(opt == 'y'):
+            # updation of price
+            print("Would you like to update the price? ")
+            print("Enter Y for Yes or N for No ")
+            option = input();
+            if( option == 'Y' ):
+                print("Existing price : ")
+                print(Sample_Space_Reader[Product]["price"])
+                print("Enter the new price : ")
+                opt = input();
+                Sample_Space_Reader[Product]["price"] = opt
+                Sample_Space_Writer = csv.DictWriter(open("test.csv", "w"))
+                Sample_Space_Writer.writerow(Sample_Space)
+
+            #updation of stock
+            print("Would you like to update the stock ? ")
+            print("Enter Y for Yes or N for No ")
+            option = input()
+            if (option == 'Y'):
+
+                option = input();
+                if (option == 'Y'):
+                    print("Existing stock : ")
+                    print(Sample_Space_Reader[Product]["stock"])
+                    print("Enter the new stock : ")
+                    opt = input();
+                    Sample_Space_Reader[Product]["stock"] = opt
+                    Sample_Space_Writer.writerow(Sample_Space)
+
+            #updation of
+        Sample_Space.close()
+        #close whatever has to be closed
+
+
+        #product is new, append
+        if(opt == 'n'):
+            with open("test.csv", "a") as Sample_Space:
+                print("Enter the stock of the product : ")
+                Product_Stock = input();
+                print("Enter the price of the product : ")
+                Product_Price = input();
+                print("Enter the ID of the stock : ")
+                Product_Id = input();
+                Sample_Space_Append = csv.writer(Sample_Space)
+                Sample_Space_Append.writerow([])
+                Sample_Space_Append.writerow([Product_Id, Product, Product_Stock, Product_Price)
+            Sample_Space.close()
+
+        return;
+
+
+
+
